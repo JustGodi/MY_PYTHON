a579685cc0219d5df4ad5b1f324a5460b60f3869
Godi <kesha006@gmail.com>
2025-09-16 16:27:55 +0300

    Work_Time

--- Code_Run/15092025--/1/main.py
+++ Code_Run/15092025--/1/main.py
@@ -6,11 +6,56 @@ def main():
     n.sort()
     return n[1]
 '''
-
-def main():
+'''
+def n_logn():
     n = list(map(int, input().split(' ')))
     n.sort()
     return n[1]
 
+print(n_logn())
+'''
+'''
+def n():
+    n = list(map(int, input().split(' ')))
+    print(n)
 
-print(main())
+
+    if n[0] > n[1] and n[0] > n[2]:
+        del n[0]
+    elif n[1] > n[0] and n[1] > n[2]:
+        del n[1]
+    elif n[2] > n[0] and n[2] > n[1]:
+        del n[2]
+
+
+    if n[0] < n[1]:
+        del n[0]
+        return n[0]
+    elif n[1] < n[0]:
+        del n[1]
+        return n[0]
+    print(n)
+
+print(n())
+'''
+
+
+def n():
+    n = list(map(int, input().split(' ')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
