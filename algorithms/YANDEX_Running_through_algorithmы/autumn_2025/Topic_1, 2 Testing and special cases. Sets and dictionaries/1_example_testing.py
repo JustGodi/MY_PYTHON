11d507dc66f32c871ef0c184d017e60b6ece3814
Godi <kesha0076@gmail.com>
2025-10-01 21:55:29 +0300

    Algoritms

--- algorithms/YANDEX_Running_through_algorithmы/autumn_2025/Topic_1, 2 Testing and special cases. Sets and dictionaries/1_example_testing.py
+++ algorithms/YANDEX_Running_through_algorithmы/autumn_2025/Topic_1, 2 Testing and special cases. Sets and dictionaries/1_example_testing.py
@@ -102,3 +102,58 @@ def square_equation(a: int, b: int, c: int):
 #square_equation(0, 0, 1) # ничего т.к. не прошло проверку b
 #square_equation(0, 0, 0) # после добавления проверки из комментария вначале # в консоль ниего не вывелось т.к. уровнение стало вида 0 = 0, а это и так истина. Но в лекции говорят про ошибку runtime error, но я хз
 '''
+
+'''
+# добавил проверку при котором все переменные равны нулю, с ответом many soluions
+def square_equation(a: int, b: int, c: int):
+    if a == 0:
+        if b != 0:
+            return print(-c/b)
+        if b == 0 and c == 0:
+            return print('Many solutions')
+    else:
+        d = b ** 2 - 4 * a * c
+        if d == 0:
+            x_1 = -b/(2 * a)
+            return print(x_1)
+        elif d > 0:
+            x_1 = (-b - (d ** (1/2)))/(2 * a)
+            x_2 = (-b + (d ** (1/2)))/(2 * a)
+            return print(x_1, x_2)
+#square_equation(1, -2, 0)
+#square_equation(1, 2, 1)
+#square_equation(1, 1, 1)
+#square_equation(0, 1, 1) # output -1, т.к. решилось линейное уравнение
+#square_equation(0, 0, 1) # ничего т.к. не прошло проверку b
+#square_equation(0, 0, 0) # Many solutions теперь всё работает
+#square_equation(-5, 4, 1) # вывод 1.0 -0.2 а должно быть -0.2, 1
+'''
+
+'''
+# добаил проверку переменных при дискриминанте боьше нуля, для корректного вывода
+def square_equation(a: int, b: int, c: int):
+    if a == 0:
+        if b != 0:
+            return print(-c/b)
+        if b == 0 and c == 0:
+            return print('Many solutions')
+    else:
+        d = b ** 2 - 4 * a * c
+        if d == 0:
+            x_1 = -b/(2 * a)
+            return print(x_1)
+        elif d > 0:
+            x_1 = (-b - (d ** (1/2)))/(2 * a)
+            x_2 = (-b + (d ** (1/2)))/(2 * a)
+            if x_1 > x_2:
+                return print(x_2, x_1)
+            else:
+                return print(x_1, x_2)
+#square_equation(1, -2, 0)
+#square_equation(1, 2, 1)
+#square_equation(1, 1, 1)
+#square_equation(0, 1, 1) 
+#square_equation(0, 0, 1) 
+#square_equation(0, 0, 0) 
+# square_equation(-5, 4, 1) # -0.2 1.0
+'''
